bf5bbeb5a9738e671d3ec4eefd0d429f11cfa044
Jia Jie Liang <dongfangjixuezhong@gmail.com>
2024-08-11 15:50:43 +0800

    add 撰寫您的第一個 C# 程式碼

---
+++ Write_your_first_Csharp_code/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World!");
+
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
